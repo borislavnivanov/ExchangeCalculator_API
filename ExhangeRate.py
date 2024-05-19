6f73827ab805509a270a3a7942736f3ddd0dd4e6
Borislav Ivanov <borislavnivanov@gmail.com>
2024-05-19 22:08:39 +0300

    implemented check for trailing and leading white spaces implemented error handling block for float explicit conversion of imputed amount implemented update on start implemented update force command and limits to not update in the same hour

--- ExhangeRate.py
+++ ExhangeRate.py
@@ -8,9 +8,15 @@ last_updated: datetime = datetime.min
 
 
 def get_api_update(base: str = 'BGN'):
+    global last_updated
+    time = datetime.now()
+    if last_updated.hour == time.hour:
+        print('Already updated, try again later')
+    else:
         api_response = requests.get(
             "https://api.currencybeacon.com/v1/latest?base=" + base + "&api_key=" + API_KEY)
         value = api_response.json()
+        last_updated = time
         return value
 
 
@@ -24,13 +30,31 @@ def calculate_exchange(_rate: float, amount: float) -> float:
     return round(amount * _rate, 2)
 
 
-while True:
-    input_currency = input('Enter Currency to compare to BGN or END for exit:\n').upper()
-    if input_currency == 'END':
-        sys.exit()
-    input_amount = float(input('Enter amount you wish to exchange:\n'))
+current_rate = get_api_update()
 
-    rates = get_api_update()
-    cur_rate = get_rate(rates, input_currency)
-    print(f'{input_amount:.2f}BGN = {calculate_exchange(cur_rate, input_amount):.2f}{input_currency}'
-          f' as of {rates['date']}')
+while True:
+    input_currency = input('Enter Currency to compare to BGN or END for exit or UPDATE to update /'
+                           'the rates:\n').upper()
+    if input_currency == 'END':
+        # implement exit option
+        sys.exit()
+    elif input_currency == 'UPDATE':
+        # force update event
+        current_rate = get_api_update()
+    # TODO: implement option to choose to change base currency
+
+    input_amount = float
+    while True:
+        try:
+            a = input('Enter amount you wish to exchange:\n').strip()
+            input_amount = float(a)
+        except ValueError:
+            print('Please enter valid floating point number!')
+            continue
+        else:
+            break
+
+    cur_rate = get_rate(current_rate, input_currency)
+    print(f'{input_amount:.2f}BGN = {calculate_exchange(cur_rate, float(input_amount)):.2f}/'
+          f'{input_currency}'
+          f' as of {current_rate['date']}')
